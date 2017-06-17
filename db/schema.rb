f5021f2276e601bb40ddbe2868e028024eb4e700
mofumofuchan <you_oki1@yahoo.co.jp>
2017-06-17 19:19:57 +0900

    Add username2 column to Room model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170617083642) do
+ActiveRecord::Schema.define(version: 20170617101850) do
 
   create_table "rooms", force: :cascade do |t|
     t.string "name", null: false
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 20170617083642) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "username"
+    t.string "username2"
   end
 
 end
