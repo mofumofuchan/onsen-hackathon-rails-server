8c5e7c78c52719547ef80612ace079a23dae118c
mofumofuchan <you_oki1@yahoo.co.jp>
2017-06-17 18:21:29 +0900

    Implement Room method

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -4,20 +4,34 @@ class RoomsController < ApplicationController
   def create
     room_param = params.permit(:name, :username)
 
-    if Room.find_by(name: room_param["name"]).nil?
+    room = Room.find_by(name: room_param["name"])
+    if room.nil?
       room = Room.new(name: room_param["name"], username: room_param["username"])
       room.save!
       render json: {"result": "created", "room_name": room.name}
       return
     else
-      render json: {"result": "connected"}
+      room.is_connected = true
+      render json: {"result": "connected", "room_name": room.name}
       return
     end
   end
 
   def show
-    render json: {result: "ok"}
-    return
+    room_param = params.permit(:name, :username)
+
+    room = Room.find_by(name: room_param["name"])
+    if room.nil?
+      render json: {"result": "not found"}, status: 404
+      return
+    end
+
+    if room.is_connected
+      render json: {"result": "connected", "room_name": room.name}
+      return
+    else
+      render json: {"result": "waiting", "room_name": room.name}
+      return
+    end
   end
 end
-ap
