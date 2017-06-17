bc617017871623ea62086ba5025aeb970f86c687
mofumofuchan <you_oki1@yahoo.co.jp>
2017-06-18 01:26:45 +0900

    Add api which returns message

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,5 +1,6 @@
 class MessagesController < ApplicationController
   protect_from_forgery except: [:create]
+  NUM_RECENT_MESSAGES = 100
 
   def create
     message_param = params.permit(:username, :roomname, :perfume, :message)
@@ -21,5 +22,21 @@ class MessagesController < ApplicationController
   end
 
   def index
+    message_param = params.permit(:roomname)
+    room = Room.find_by(name: message_param["roomname"])
+    if room.nil?
+      render json: {"result": "room not found"}, status: 404
+      return
+    end
+    messages = Message.where(room: room).
+               order("updated_at DESC").limit(NUM_RECENT_MESSAGES)
+    messages_json = messages.map do |m|
+      {"username": m.username,
+       "message": m.message,
+       "perfume": m.perfume
+      }
+    end
+    render json: messages_json
+    return
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
     scope 'v0' do
       scope 'rooms' do
         post '/:roomname', to: "messages#create"
+        get '/:roomname', to: "messages#index"
         post '/', to: "rooms#create"
         get '/', to: "rooms#show"
       end
