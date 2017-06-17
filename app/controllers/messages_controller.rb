5f9c4c69a50e4ed2e82dd831702f271ab440cc28
mofumofuchan <you_oki1@yahoo.co.jp>
2017-06-17 19:42:50 +0900

    Implement posting messages api

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,7 +2,21 @@ class MessagesController < ApplicationController
   protect_from_forgery except: [:create]
 
   def create
-    render json: {"result":" messages post ok"}
+    message_param = params.permit(:username, :roomname, :perfume, :message)
+
+    message = Message.new()
+    message.username = message_param["username"]
+    room = Room.find_by(name: message_param["roomname"])
+    if room.nil? 
+      render json: {"result": "room not found"}, status: 404
+      return
+    end
+    message.room = room
+    message.message = message_param["message"]
+    message.perfume = message_param["perfume"]
+    message.save!
+    
+    render json: {"result": "created"}, status: 201
     return
   end
 
